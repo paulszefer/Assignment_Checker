1b7937a30bb09433d9d6f449a89855184ef66bc0
paulszefer <pszefer@hotmail.com>
2017-07-18 02:13:59 -0700

    Initial functionality commit
    AssignmentChecker.py will process assignments by extracting the sources,
    running checkstyle against the sources and generating a checkstyle report,
    compiling the sources and generating an error report.
    
    Further functionality will include executing the compiled files with file
    input if needed, recording of system output, and handling of incorrectly
    formatted submission zip files.

---
+++ AssignmentChecker.py
@@ -0,0 +1,64 @@
+# ------------------------------------------------------------------
+# AssignmentChecker.py
+#
+# By: Paul Szefer
+#
+# This program processes Java assignments.
+#
+# The assignment will consist of a zip folder containing one or
+# more packages of Java projects.
+#
+# 1. A new folder named after the UserID will be created in the
+# same directory as the zip folder.
+# 2. The source files will be extracted from the zip folder
+# into a src folder within the UserID folder, maintaining their
+# package structure.
+# 3. Checkstyle will be run across all of the Java source files
+# and a report named checkstyle_report will be created in the
+# UserID folder.
+# 4. The source files will be compiled into a bin folder inside
+# the UserID folder, with a file named compile_errors storing
+# all errors occurring during compilation.
+# 5. Each source file package will be executed.
+#
+# Assignments have the following structure:
+#
+#   (UserID).zip
+#       |
+#       |-------(package1)------(java sources), (other resources)
+#       |
+#       |-------(package2)------(java sources), (other resources)
+#       |
+#       |-------(package3)------(java sources), (other resources)
+#       |
+#       ...
+#       |
+#       readme.txt
+#
+# TODO - redirection for file input, system output
+# TODO - check for/handle incorrect submission zip format
+#
+# ------------------------------------------------------------------
+
+from pathlib import Path
+import zipfile
+from Helper import *
+
+
+# The point of entry into the program.
+def main():
+    for zipFolder in filter(is_zip_folder, Path().iterdir()):
+        folder_name = zipFolder.name.rstrip('.zip')
+
+        with zipfile.ZipFile(str(zipFolder)) as this_zip:
+            extract_sources(folder_name, this_zip)
+
+            run_checkstyle(folder_name)
+
+            compile_sources(folder_name)
+
+            # execute_projects()
+
+
+# Begins execution with the main() method
+main()
---
+++ Helper.py
@@ -0,0 +1,77 @@
+import os
+import subprocess
+
+
+# Returns true if the given path refers to a zip folder
+def is_zip_folder(path):
+    return has_extension(path, 'zip')
+
+
+# Returns true if the given path refers to a Java source file
+def is_java_source_file(path):
+    return has_extension(path, 'java')
+
+
+# Returns true if the given path refers to a file with the given extension
+def has_extension(path, ext):
+    return path.name.endswith(ext)
+
+
+# Extracts the Java source files from the given zip folder to a src folder
+# in a folder with the given folder name.
+def extract_sources(folder_name, zip_folder):
+    for _ in zip_folder.filelist:
+        zip_folder.extractall(folder_name + '/src/')
+
+
+# Runs checkstyle on all Java source files found in the src directory and
+# generates a report with the output in the same directory as the src
+# folder.
+#
+# The checkstyle jar used is: checkstyle-8.0-all.jar
+# The checkstyle configuration file used is: checkstyle_config.xml
+# The report file generated is: checkstyle_report.txt
+def run_checkstyle(folder_name):
+    command = subprocess.Popen(
+        'java -jar checkstyle-8.0-all.jar -c checkstyle_config.xml -o ' + folder_name + '/checkstyle_report.txt ' + folder_name + '/src/*',
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    output, errors = command.communicate()
+    print('Checkstyle output: ' + trim_byte_chars(str(output)))
+    print('Checkstyle errors: ' + trim_byte_chars(str(errors)))
+
+
+# Compiles all Java source files found in the src directory to a bin
+# folder a folder with the given folder name. Compilation errors
+# are recorded in a file in the same directory
+# as the bin folder.
+#
+# The report file generated is: compile_errors.txt
+def compile_sources(folder_name):
+    subprocess.Popen('get_sources.bat ' + folder_name)
+
+    os.makedirs(str(folder_name).replace('.zip', '') + '/bin/', exist_ok=True)
+    command = subprocess.Popen(
+        'javac -d ' + folder_name + '/bin/ @' + folder_name + '/sources.txt',
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    output, errors = command.communicate()
+    output_str = trim_byte_chars(str(output))
+    errors_str = trim_byte_chars(str(errors))
+    print('Compile output: ' + output_str)
+    print('Compile errors: ' + errors_str)
+
+    error_log = open(folder_name + '/compile_errors.txt', 'w')
+    if errors_str == "":
+        error_log.write('No errors')
+    else:
+        error_log.write(errors_str)
+    error_log.close()
+
+
+# Returns a string with the byte characters removed.
+# These byte characters consist of:
+#   b' to start the string
+#   '  to end the string
+def trim_byte_chars(string):
+    return string.lstrip('b\'').rstrip('\'')
